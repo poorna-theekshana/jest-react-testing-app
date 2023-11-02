a595392d36d4dd121c14966b5dbc7a70ea9004d6
Poorna Theekshana <poornatheekshana@outlook.com>
2023-11-02 11:06:25 +0530

    Snapshot Testing

--- jest-react-testing-app/src/App.js
+++ jest-react-testing-app/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const STATUS = {
+  HOVERED: "hovered",
+  NORMAL: "normal",
+};
+
+export default function App() {
+  const [status, setStatus] = useState(STATUS.NORMAL);
+
+  const onMouseEnter = () => {
+    setStatus(STATUS.HOVERED);
+  };
+
+  const onMouseLeave = () => {
+    setStatus(STATUS.NORMAL);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={status}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      Hover over me
     </div>
   );
 }
-
-export default App;
